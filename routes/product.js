e29411953dbaf1c42cb75dab1d2f6ac927c667f7
pisitsusuk <pisitsusuk@gmail.com>
2025-09-27 20:25:28 +0700

    แก้บรรทักที่ 6

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const { authCheck, adminCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require("../middlewares/authCheck");
 
 const {
   create,
