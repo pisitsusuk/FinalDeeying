da3e3f620cc52c1f45bd4731a28204a049d97081
pisitsusuk <pisitsusuk@gmail.com>
2025-09-27 20:07:30 +0700

    แอดมิน “ลบผู้ใช้ไม่ได้” ถ้าเคยมีออเดอร์ 3,15,26

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authCheck /*, adminCheck*/ } = require("../middlewares/authCheck");
+const { authCheck , adminCheck } = require("../middlewares/authCheck");
 
 const {
   listUsers,
